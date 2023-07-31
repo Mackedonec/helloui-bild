3c617c58ef9adf0388b8e12d3643ecac4c4ffa68
Mackedonec <mackedonec187@gmail.com>
2023-07-31 12:28:56 +0300

    add navigation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -42,3 +42,14 @@ $(document).ready(function () {
     animationDuration: 600,
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navLinks.forEach((navLink) => navLink.classList.remove("active"));
+
+      this.classList.add("active");
+    });
+  });
+});
